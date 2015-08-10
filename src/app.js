e6ddeb4b508143776395f851bde549889d02d724
willowcheng <e.wingus@Gmail.com>
2015-08-10 16:44:55 -0400

    Sine and cosine usage

--- src/app.js
+++ src/app.js
@@ -4,33 +4,52 @@ var HelloWorldLayer = cc.Layer.extend({
         //////////////////////////////
         // 1. super init first
         this._super();
-
         var size = cc.winSize;
 
-        var rect = new cc.DrawNode();
-        rect.drawRect(cc.p(0, 0), cc.p(300, 300), cc.color(), 5, cc.color(255, 0, 0, 255));
+        /**
+         *
+         * Global and local coordinate system
 
-        var dot = new cc.DrawNode();
-        dot.drawDot(cc.p(0, 0), 10, cc.color(255, 255, 255, 255));
+         var size = cc.winSize;
 
-        this.addChild(rect);
-        rect.addChild(dot);
+         var rect = new cc.DrawNode();
+         rect.drawRect(cc.p(0, 0), cc.p(300, 300), cc.color(), 5, cc.color(255, 0, 0, 255));
 
-        rect.setPositionX(size.width / 2);
-        rect.setPositionY(size.height / 2);
+         var dot = new cc.DrawNode();
+         dot.drawDot(cc.p(0, 0), 10, cc.color(255, 255, 255, 255));
 
-        dot.setPositionX(10);
-        dot.setPositionY(10);
+         this.addChild(rect);
+         rect.addChild(dot);
 
-        rect.setContentSize(cc.size(300, 300));
-        rect.setAnchorPoint(cc.p(0.5, 0.5));
+         rect.setPositionX(size.width / 2);
+         rect.setPositionY(size.height / 2);
 
-        this.schedule(function() {
+         dot.setPositionX(10);
+         dot.setPositionY(10);
+
+         rect.setContentSize(cc.size(300, 300));
+         rect.setAnchorPoint(cc.p(0.5, 0.5));
+
+         this.schedule(function() {
             rect.setRotation(rect.getRotation() + 1);
 
             var p = dot.convertToWorldSpace(cc.p(0, 0));
             cc.log("(", p.x, ",", p.y, ")");
 
+        });
+         */
+
+        var dot = new cc.DrawNode();
+        dot.drawDot(cc.p(0, 0), 10, cc.color(255, 255, 255, 255));
+        this.addChild(dot);
+
+        dot.setPositionX(size.width / 2);
+        dot.setPositionY(size.height / 2);
+        var angle = 0;
+        this.schedule(function () {
+            dot.setPositionX(size.width / 2 + Math.sin(angle) * 100);
+            dot.setPositionY(size.height / 2 + Math.cos(angle) * 150);
+            angle += 0.1;
         });
 
         return true;
